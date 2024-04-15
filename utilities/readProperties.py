e07e387a58b8c647d2d95a78dbca0bcb3254a713
itsmasad <muhammadasadofficial@gmail.com>
2024-04-15 19:07:56 +0500

    created utilities file

---
+++ utilities/readProperties.py
@@ -0,0 +1,22 @@
+# This file will read the data from ini (config.ini) file
+import configparser
+
+config = configparser.RawConfigParser()
+config.read(".\\Configurations\\config.ini")
+
+class ReadConfig():
+    #Creating staticmethod so we can directly acces using class name without creating any object
+    @staticmethod
+    def getApplicationURL():
+        url = config.get('common info','baseURL')
+        return url
+
+    @staticmethod
+    def getUseremail():
+        username = config.get('common info','username')
+        return username
+        
+    @staticmethod
+    def getUserpassword():
+        password = config.get('common info','password')
+        return password
