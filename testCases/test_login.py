715daabffae5c38769d77c46761dbaeb510132e2
itsmasad <muhammadasadofficial@gmail.com>
2024-04-15 18:15:11 +0500

    updated test_login.py

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,3 +1,8 @@
 import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
+
+class Test_001_Login:
+    baseURL = "https://admin-demo.nopcommerce.com/login"
+    username = "admin@yourstore.com"
+    password = "admin"
