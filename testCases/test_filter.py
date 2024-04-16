c1ef0885e9c9833fc8bf3a43451de65ba9c2f9d2
itsmasad <muhammadasadofficial@gmail.com>
2024-04-16 17:21:38 +0500

    fixing issue

--- testCases/test_filter.py
+++ testCases/test_filter.py
@@ -1,20 +1,32 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pageObjects.Manu import Manu
+from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
 
 
 class TestCase_002_Products:
     baseURL = ReadConfig.getApplicationURL
     productName = ReadConfig.getProductFilterData
+    userName = ReadConfig.getUseremail
+    password = ReadConfig.getUserpassword
 
     def test_Product_filter(self,setUp):
 
         self.driver = setUp
         self.driver.get(self.baseURL)
         self.Manu = Manu(self.driver)
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setUserName(self.userName)
+        self.lp.setPassword(self.password)
+        self.lp.ClickLogin()
+
         self.Manu.Click_manue_catalog()
         self.Manu.Click_manue_products()
         self.Manu.setProductName(self.productName)
         self.Manu.Click_searchButton()
+        time.sleep(3)
+        self.driver.close()
