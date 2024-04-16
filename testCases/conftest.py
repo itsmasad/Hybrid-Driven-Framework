4b8725283ffa056cf4b41927e7f986e9bd935e00
itsmasad <muhammadasadofficial@gmail.com>
2024-04-16 16:17:01 +0500

    added conditions on selecting the browser from CLI

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,17 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def setUp():
-    driver = webdriver.Chrome()
+def setUp(browser):
+    if browser=='chrome':
+        driver = webdriver.Chrome()
+    elif browser =='firefox':
+        driver = webdriver.Firefox()
+        
     return driver
+
+def pytest_addoption(parser): #This will get the value from CLI /hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request): # This will return the browser value to the setup method
+    return request.config.getoption("--browser")
