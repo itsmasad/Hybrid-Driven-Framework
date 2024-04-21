b90401cafffae9f05c411ae3b00f7bb032003ee5
itsmasad <muhammadasadofficial@gmail.com>
2024-04-21 18:35:33 +0500

    updated reports

--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -1,7 +1,10 @@
 # This file will read the data from ini (config.ini) file
+# configparser module provides functionality to work with configuration files.
 import configparser
 
+# Creating an instance of RawConfigParser class called config
 config = configparser.RawConfigParser()
+# Reading the contents of the configuration file named config.ini
 config.read(".\\Configurations\\config.ini")
 
 class ReadConfig():
