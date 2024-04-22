17159eb3cabb9ea3867d31c69079341c7cee0eb2
itsmasad <muhammadasadofficial@gmail.com>
2024-04-22 13:27:02 +0500

    updated file

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -13,6 +13,7 @@ class LoginPage:
     def __init__(self,driver):
         self.driver = driver
 
+
     def setUserName(self,username):
         self.driver.find_element(By.ID,self.textbox_username_id).clear()
         self.driver.find_element(By.ID, self.textbox_username_id).send_keys(username)
